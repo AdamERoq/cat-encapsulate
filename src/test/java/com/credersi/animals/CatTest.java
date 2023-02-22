2ffeda182244f75c6429c749ec97599a38eaaf17
Adam Eckley <adam.eckley@roq.co.uk>
2023-02-22 10:52:21 +0000

    Added method to allow the cat to have nine lives and added a two tests to check it is working.

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -1,6 +1,7 @@
 package com.credersi.animals;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,4 +13,57 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testCatRuns() {
+		Cat cat = new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	
+	@Test
+	public void testCatDead() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assertTrue(cat.isAlive());
+	}
+	
+	@Test 
+	public void testCatFullHealth() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+		}
+	
+	@Test
+	public void testCatDies() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assertTrue(cat.isAlive());
+		}
+	
+	@Test
+	public void testKillCatNineTimes() {
+		Cat cat = new Cat();
+		for (int i = 0; i <= 8; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertFalse(cat.isAlive());
+	}
+	
+	@Test
+	public void testKillCatEightTimes() {
+		Cat cat = new Cat();
+		for (int i = 0; i <= 7; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertTrue(cat.isAlive());
+	}
+	
 }
